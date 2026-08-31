9fe9c7d12ab11e2218df349ea33dbc3134e5f79a
asdefghasdefgh-sketch <asdefghasdefgh@gmail.com>
2026-08-30 21:11:02 -0300

    version 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/Cuestionario-Inicial-Nutricion-German/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
